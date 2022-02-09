6c7f6e46621f13f64e252f347689de9ab8f81b91
pablokumar1993 <pablokumar1993@gmail.com>
2022-02-09 20:04:27 +0100

    crear persona e imprimir por pantalla

--- src/git/main/Main.java
+++ src/git/main/Main.java
@@ -1,10 +1,14 @@
 package git.main;
 
+import git.entidad.Persona;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Bienvenidos a nuestras versiones deon GIT");
         System.out.println("Esto es otro cambio en el proyecto");
+        Persona p = new Persona("Steve Rogers", 45);
+        System.out.println(p);
     }
 
 }
