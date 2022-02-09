bc1204bc5b31c0580fd7f79312934f7244fbc8e8
pablokumar1993 <pablokumar1993@gmail.com>
2022-02-09 19:26:32 +0100

    añadida salida pantalla

--- src/git/main/Main.java
+++ src/git/main/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Bienvenidos a nuestras versiones deon GIT");
+        System.out.println("Esto es otro cambio en el proyecto");
     }
 
 }
